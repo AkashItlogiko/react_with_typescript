c21e80397fca8481d7534ebad7554358547d8d2c
AkashItlogiko <akashsahanwu089@gmail.com>
2025-01-26 01:03:29 +0600

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import User from './component/User';
 
 function App() {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>React Typescript</h1>
+      <User/>
     </div>
   );
 }
