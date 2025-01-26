3b91f61b8a87b17fd150207fd94c19dbec3f17d1
AkashItlogiko <akashsahanwu089@gmail.com>
2025-01-27 01:16:38 +0600

    Done

--- src/component/User.tsx
+++ src/component/User.tsx
@@ -10,6 +10,7 @@ type UserProps = {
 const User = ({ user }: UserProps) => {
   return (
     <div style={{ textAlign: 'center', border: '1px solid', margin: '0.5rem' }}>
+     
       <h2>{user.name}</h2>
       <h2>{user.age} years old</h2>
       {user.isRegisterd ? <p>Registered User</p> : <p>Not Registerd User</p>}
